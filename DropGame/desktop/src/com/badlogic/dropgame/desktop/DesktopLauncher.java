d30a31c6335de3812b8ff068878e211ba03604ee
gerser2000 <nomengerard@gmail.com>
2021-01-14 19:08:53 +0100

    firstCommit

---
+++ DropGame/desktop/src/com/badlogic/dropgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badlogic.dropgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.dropgame.DropGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DropGame(), config);
+	}
+}
